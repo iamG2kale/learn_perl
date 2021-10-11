3fb93743d8e38d7ccac526a8eafaa095bc7b1ca0
iamG2kale <jitesh.kale96@gmail.com>
2021-10-11 19:10:19 +0530

    subroutine file

---
+++ subroutines.pl
@@ -0,0 +1,31 @@
+use strict;
+use warnings;
+my @myArr = ("google.com 100", "yahoo.com 200", "microsoft.com 101", "git.org 300", "apple.co.in 400");
+
+##subroutine to add element
+sub print_sub{
+    my $arg1 = shift;
+    my $arg2 = shift;
+    push(@myArr,$arg1." ".$arg2);
+    print(join(" ",@myArr));
+}
+print_sub("img2.xyz", "500");
+
+##subroutine to append domain
+
+sub addDomain{
+    my @myArr = @_;
+    foreach (@myArr){
+        if ($_ =~ /com/){
+            $_.=": COM";
+        }
+        else{
+            $_.=": ORG";
+        }
+    }
+    return(@myArr);
+
+}
+
+@myArr = addDomain(@myArr);
+print(@myArr,"\n");
